a0e26e1793e742c4e6f68f3c79681658268e8483
kavya avula <154750014+kavya-avula@users.noreply.github.com>
2026-08-18 23:29:22 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1603. Running Sum of 1d Array/Solution.java
+++ LeetCode/Java/Easy/1603. Running Sum of 1d Array/Solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<=i;j++){
-                ans[i]+=nums[j];
-            }
+        for(int i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
         }
-        return ans;
+        return nums;
     }
 }
