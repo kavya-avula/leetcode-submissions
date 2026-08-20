bc4e77ee47142ca2b6c745d65aee7c01809d5e53
kavya avula <154750014+kavya-avula@users.noreply.github.com>
2026-08-20 22:31:36 +0530

    Add Java Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx=0;
+        int dec=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[idx]=nums[i];
+                idx++;
+            }
+        }
+        for(int i=idx;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
